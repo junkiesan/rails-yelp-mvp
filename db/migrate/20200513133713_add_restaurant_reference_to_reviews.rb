069d1b4323dcbf5a7697efa5b645113e5a5907ac
Julien Bouland <bouland.julien@gmail.com>
2020-05-13 15:45:28 +0200

    Add address to Restaurant

--- db/migrate/20200513133713_add_restaurant_reference_to_reviews.rb
+++ db/migrate/20200513133713_add_restaurant_reference_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddRestaurantReferenceToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_reference :reviews, :restaurants, foreign_key: true
+    add_reference :reviews, :restaurant, foreign_key: true
   end
 end
