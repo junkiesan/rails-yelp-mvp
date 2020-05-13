08d3871c0849b6e5a10d78d0bf028dee0050bdf9
Julien Bouland <bouland.julien@gmail.com>
2020-05-13 19:07:01 +0200

    app finaly done need some front

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.save
+    @review = Review.new
   end
 
   def create
@@ -19,6 +19,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
